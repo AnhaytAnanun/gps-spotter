9d1175d243812007003ea098236efd10d5461796
NickSargsyan <nicksargsyan@yahoo.com>
2014-04-18 16:16:43 +0400

    storage txt

--- api/v1/handlers/satellite.js
+++ api/v1/handlers/satellite.js
@@ -1,10 +1,23 @@
 // var mongoose = require('mongoose');
 // var Satellite = mongoose.model('Satellite');
 
+var fs = require('fs');
+var path = require('path');
+
 exports.get = function (req, res, next) {
-	res.send(200, 'get');
+	var dirname = path.resolve(__dirname, '../../..');
+	dirname = path.join(dirname, '/storage/satellites.txt');
+	fs.readFile(dirname, 'utf8', function(err, satellites) {
+        console.error('GET /satellites', err);
+        res.send(200, JSON.parse(satellites));
+    });
 };
 
 exports.add = function (req, res, next) {
-	res.send(200, 'add');
+	var dirname = path.resolve(__dirname, '../../..');
+	dirname = path.join(dirname, '/storage/satellites.txt');
+	fs.readFile(dirname, 'utf8', function(err, satellites) {
+        console.error('POST /satellites', err);
+        res.send(201);
+    });
 };
--- schemas/satellite.js
+++ schemas/satellite.js
@@ -3,7 +3,19 @@ var mongoose = require('mongoose')
 	, ObjectId = Schema.ObjectId;
  
 var satelliteSchema = new Schema({
-
+	coordinate: {
+		longitude: {type: Number, required: true},
+		lattitude: {type: Number, required: true}
+	},
+	info: {
+		car: {
+			type: {type: String},
+			color: {type: String},
+			model: {type: String}
+		},
+		phone: {type: String},
+		name: {type: String}
+	}
 });
  
 module.exports = mongoose.model('Satellite', satelliteSchema);
